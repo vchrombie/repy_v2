e2814bb1384795364b96e03b958ecdecce608176
Venu Vardhan Reddy Tekula <vt2182@nyu.edu>
2023-11-14 19:10:03 -0500

    update the scripts

--- scripts/TARGET_FOLDER/fetch-rm-writeup-netid.py
+++ scripts/TARGET_FOLDER/fetch-rm-writeup-netid.py
@@ -9,8 +9,8 @@ reference_monitors_3_path = "2.3_reference_monitors"
 json_file_path = 'netid-data.json'
 
 # Function to search for files and return a list of paths
-def find_files(directory, extension):
-    return [os.path.join(directory, f) for f in os.listdir(directory) if f.endswith(extension)]
+def find_files(directory, format):
+    return [os.path.join(directory, f) for f in os.listdir(directory) if format in f]
 
 # Function to create the dictionary from filenames
 def create_dict(ref_monitors_1, ref_monitors_3, writeups):
@@ -18,7 +18,7 @@ def create_dict(ref_monitors_1, ref_monitors_3, writeups):
     for ref_monitor in ref_monitors_1:
         netid = os.path.splitext(ref_monitor)[0].split('_')[-1]
         data[netid] = {'reference_monitor_1': os.path.basename(ref_monitor)}
-    
+
     for ref_monitor in ref_monitors_3:
         netid = os.path.splitext(ref_monitor)[0].split('_')[-1]
         if netid in data:
@@ -52,11 +52,13 @@ def open_in_vscode(file_path):
         print("Error: ", file_path, " does not exist")
 
 # Function to run reference monitor against base attack case
-def run_reference_monitor_base_test(ref_monitor):
+def run_reference_monitor_base_test(reference_monitors_path, ref_monitor):
     print("Running reference monitor against vt2182_attackcase2.r2py")
-    call(["cd", reference_monitors_path])
-    call(["rr2", ref_monitor, "vt2182_attackcase2.r2py"])
-    call(["cd", ".."])
+    os.chdir(reference_monitors_path)
+    # run cat ref_monitor
+    os.system(f"rr2 {ref_monitor} vt2182_attackcase2.r2py")
+    os.system(f"cd ..")
+    print(os.getcwd())
 
 
 # Check if JSON file exists and is not empty
@@ -70,7 +72,7 @@ if os.path.exists(json_file_path) and os.path.getsize(json_file_path) > 0:
         print(f"Name: {name}")
         ref_monitor_1 = data[netid][1]['reference_monitor_1']
         ref_monitor_3 = data[netid][2]['reference_monitor_3']
-        writeup = data[netid][2]['writeup']
+        writeup = data[netid][3]['writeup']
 
         # Check if the reference monitor or writeup exists and print message accordingly
         ref_monitor_1_path = f"{reference_monitors_1_path}/{ref_monitor_1}" if ref_monitor_1 else "No submission for reference monitor 2.1 assignment"
@@ -78,20 +80,23 @@ if os.path.exists(json_file_path) and os.path.getsize(json_file_path) > 0:
         writeup_path = f"{writeups_path}/{writeup}" if writeup else "No submission for writeup"
 
         if not ref_monitor_1:
-            print("No submission for reference monitor 2.1 assignment")
+            print("No submission for reference monitor 2.1 assignment ", name)
         else:
             open_in_vscode(ref_monitor_1_path)
 
-        # Run the reference monitor against base attack case
-        run_reference_monitor_base_test(ref_monitor_1)
+            # Run the reference monitor against base attack case
+            run_reference_monitor_base_test(reference_monitors_1_path, ref_monitor_1)
 
         if not ref_monitor_3:
-            print("No submission for reference monitor 2.3 assignment")
-        # else:
-        #     open_in_vscode(ref_monitor_3_path)
+            print("No submission for reference monitor 2.3 assignment ", name)
+        else:
+            open_in_vscode(ref_monitor_3_path)
+
+            # Run the reference monitor against base attack case
+            run_reference_monitor_base_test(reference_monitors_3_path, ref_monitor_3)
 
         if not writeup:
-            print("No submission for writeup")
+            print("No submission for writeup ", name)
         else:
             if writeup.endswith('.docx'):
                 call(["open", writeup_path])
@@ -99,11 +104,11 @@ if os.path.exists(json_file_path) and os.path.getsize(json_file_path) > 0:
                 open_in_vscode(writeup_path)
 
     else:
-        print("No data found for the provided netid.")
+        print("No data found for the provided netid ", netid)
 else:
     # If the JSON file is empty or doesn't exist, populate it
-    ref_monitors_1 = find_files(reference_monitors_1_path , '.r2py')
-    ref_monitors_3 = find_files(reference_monitors_3_path , '.r2py')
+    ref_monitors_1 = find_files(reference_monitors_1_path , 'reference_monitor_')
+    ref_monitors_3 = find_files(reference_monitors_3_path , 'reference_monitor_')
     writeups = find_files(writeups_path, '.docx') + find_files(writeups_path, '.pdf')
 
     # Create dictionary
